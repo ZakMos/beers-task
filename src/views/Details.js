aa28a0a0dcc56a1ed6493ac1d7823c51a4882d89
Zakaria Moslim <z.zza@hotmail.com>
2019-04-15 23:09:47 +0200

    add main page with detail, only setup without data

---
+++ src/views/Details.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Details extends Component {
+  render() {
+    return (
+      <p>Wilcome to details</p>
+    );
+  }
+}
+
+
+export default Details;
