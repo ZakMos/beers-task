f0386f1e0443e05c677367f70310fe24afa6114c
Zakaria Moslim <z.zza@hotmail.com>
2019-04-15 23:10:03 +0200

    inclode all files and update app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Main from './views/Main';
+import Details from './views/Details';
+import NotFound from './views/NotFound';
+import Footer from "./components/Footer";
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state={};
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-
-        </header>
-        <p>Hello Beers!</p>
-      </div>
+      <BrowserRouter>
+        <Fragment>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Main} />
+              <Route exact path="/details" component={Details} />
+              <Route component={NotFound} />
+            </Switch>
+          </main>
+          <Footer />
+        </Fragment>
+      </BrowserRouter>
     );
   }
 }
